63c24ab5b3ae35f9c14b287f8ec973510d351c8e
Flatice <davidespanti@gmail.com>
2025-01-27 16:33:12 +0100

    Start menu
    Started working on the start menu, still a lot to do

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     GameObject gameOverScreen, startMenu, pauseMenu;
+    Button buttonStartGame; 
 
     [SerializeField]
     float gameOverDelayTime = 1f;
@@ -14,9 +16,14 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         gameOverScreen = transform.Find("Game Over Screen").gameObject;
+        startMenu = transform.Find("Menu Start").gameObject;
 
+        buttonStartGame = startMenu.transform.Find("Button start game").gameObject.GetComponent<Button>();
+        // FIXME: useless here I think, use OnPointerDown in a script attached to the Button
+        
         // all menus exists at startup, they are activated and deactivated wheen needed
         gameOverScreen.SetActive(false);
+        // Menu Start is already active at startup
 
     }
 
